a2974e5e7bc39ce494b6d3f802f82aaf57e94294
JM O'Brien <83389601+jmobriencs@users.noreply.github.com>
2022-11-20 11:34:17 -0800

    Update 3060ti FE Stock Checker.py

--- 3060ti FE Stock Checker.py
+++ 3060ti FE Stock Checker.py
@@ -12,19 +12,19 @@ while True:
     driver = webdriver.Chrome(service=service)
 
     # enter link of item to check
-    driver.get("https://www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics-card-steel-and-black/6439402.p?skuId=6439402")
+    driver.get("URL GOES HERE")
 
     print(driver.title)
     el = driver.find_element(By.TAG_NAME, "body")
     str = el.text
 
     if (str.find("Sold Out") != -1):
-        print("3060ti is still not available at Best Buy")
+        print("ITEM NAME HERE is still not available.")
         driver.close()
         time.sleep(10) # program checks every 10 seconds if item is in stock
     else:
         # create a Twilio account and get a number through them (free)
         print("3060ti IS NOW AVAILABLE Best Buy")
-        client.messages.create(to="+yourCellNumber", from_="+yourTwilioNumber", body="3060ti is now available!!! Go to: www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics-card-steel-and-black/6439402.p?skuId=6439402")
+        client.messages.create(to="+yourCellNumber", from_="+yourTwilioNumber", body="ITEM NAME HERE is now available!!! Go to: INSERT URL HERE")
         driver.close()
         time.sleep(300) # program will resend text notification every 5 minutes if item is in stock
